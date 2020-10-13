b1eae19bfc004454677808956d899056c7885b93
uzimike <michaelktlr78@gmail.com>
2020-10-13 14:46:45 +1300

    epic appearance improvements all across the board

---
+++ src/constants.js
@@ -0,0 +1,32 @@
+export const casings = [
+  {
+    label: 'Original',
+    value: 'original',
+    key: 'original',
+  },
+  {
+    label: 'lowercase',
+    value: 'lowercase',
+    key: 'lowercase',
+  },
+  {
+    label: 'UPPERCASE',
+    value: 'uppercase',
+    key: 'uppercase',
+  },
+  {
+    label: 'Capitalised',
+    value: 'capitalized',
+    key: 'capitalized',
+  },
+  {
+    label: 'Sentence',
+    value: 'sentence',
+    key: 'sentence',
+  },
+  {
+    label: 'rANdoMiseD',
+    value: 'random',
+    key: 'random',
+  },
+];
